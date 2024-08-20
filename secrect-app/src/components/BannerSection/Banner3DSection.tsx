05f6dfa1e38b19936d20e10fa6b7a32b94fc50f6
PhatTurtle <phatrua2911@gmail.com>
2024-08-20 11:16:29 +0700

    updfaet

--- secrect-app/src/components/BannerSection/Banner3DSection.tsx
+++ secrect-app/src/components/BannerSection/Banner3DSection.tsx
@@ -4,7 +4,7 @@ import Spline  from '@splinetool/react-spline';
 function Banner3DSection() {
     return (
         <div className='mt-20 w-full'>
-          <Spline className='bg-transparent' scene='https://prod.spline.design/Qj91K-OJPRB9yaka/scene.splinecode'></Spline>  
+          <Spline className='bg-transparent' scene='https://prod.spline.design/97pxr-RT7zmqFk19/scene.splinecode'></Spline>  
         </div>
     );
 }
